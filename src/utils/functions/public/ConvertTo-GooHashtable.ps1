8b6ddcf662c1502c24c2da227fcd8bd0fce27bd7
daLLyw0w <stefanbuciujr@gmail.com>
2021-04-01 20:43:56 +0300

    released utils 0.2.2-alpha

--- src/utils/functions/public/ConvertTo-GooHashtable.ps1
+++ src/utils/functions/public/ConvertTo-GooHashtable.ps1
@@ -4,31 +4,15 @@ function ConvertTo-GooHashtable {
     [OutputType([hashtable])]
     param (
         [Parameter(Mandatory = $true, Position = 0, ValueFromPipeline = $true)]
-        [PSCustomObject]
-        $PSCustomObject
+        [psobject]
+        $InputObject,
+
+        [Parameter(Mandatory = $false)]
+        [uint]
+        $Depth = 2
     )
-    begin {
-        $q = [System.Collections.Generic.Queue[System.Tuple[[hashtable], [PSCustomObject]]]]::new()
-    }
 
     process {
-        $root = @{}
-        $q.Enqueue([System.Tuple[[hashtable], [PSCustomObject]]]::new($root, $PSCustomObject))
-        while (0 -lt $q.Count) {
-            [System.Tuple[[hashtable], [PSCustomObject]]]$item = $q.Dequeue()
-            foreach ($nvp in $item.Item2.PSObject.Properties) {
-                if ($nvp.Value -is [PSCustomObject]) {
-                    $value = @{}
-                    $q.Enqueue([System.Tuple[[hashtable], [PSCustomObject]]]::new($value, $nvp.Value))
-                }
-                else {
-                    $value = $nvp.Value
-                }
-
-                $item.Item1.($nvp.Name) = $value
-            }
-        }
-
-        return $root
+        return $InputObject | ConvertTo-Json -Depth $Depth | ConvertFrom-Json -AsHashtable
     }
 }
---
+++ test/utils/functions/public/ConvertTo-GooFlattenHashtable.Tests.ps1
@@ -0,0 +1,84 @@
+
+Describe 'ConvertTo-GooFlattenHashtable' {
+
+    It 'Convert simple object' {
+        # Arrange
+        $expected = @{
+            a = 1;
+            b = 'two';
+            c = $true;
+            d = 12.34;
+        }
+
+        $pso = [PSCustomObject]$expected
+
+        # Act
+        $actual = $pso | ConvertTo-GooFlattenHashtable
+        $eqs = $actual | Compare-Object -ReferenceObject $expected -Property a, b, c, d -IncludeEqual -CaseSensitive | Select-Object -ExpandProperty SideIndicator
+
+        # Assert
+        $eqs | ForEach-Object { $_ | Should -Be '==' }
+    }
+
+    It 'Convert depth 1 object' {
+        # Arrange
+        $expected = @{
+            a     = 1;
+            b     = 'two';
+            c     = $true;
+            d     = 12.34;
+            'e.f' = 2;
+            'e.g' = 3;
+        }
+
+        # Act
+        $actual = ([PSCustomObject]@{
+                a = 1;
+                b = 'two';
+                c = $true;
+                d = 12.34;
+                e = [PSCustomObject]@{
+                    f = 2;
+                    g = 3;
+                }
+            }) | ConvertTo-GooFlattenHashtable
+        $eqs = $actual | Compare-Object -ReferenceObject $expected -Property a, b, c, d, 'e.f', 'e.g' -IncludeEqual -CaseSensitive | Select-Object -ExpandProperty SideIndicator
+        
+        # Assert
+        $eqs | ForEach-Object { $_ | Should -Be '==' }
+    }
+
+    It 'Convert depth 2 object' {
+        # Arrange
+        $expected = @{
+            a       = 1;
+            b       = 'two';
+            c       = $true;
+            d       = 12.34;
+            'e.f'   = 2;
+            'e.g'   = 3;
+            'e.h.i' = 123.123;
+            'e.h.j' = '123.123';
+        }
+
+        # Act
+        $actual = ([PSCustomObject]@{
+                a = 1;
+                b = 'two';
+                c = $true;
+                d = 12.34;
+                e = [PSCustomObject]@{
+                    f = 2;
+                    g = 3;
+                    h = [PSCustomObject]@{
+                        i = 123.123;
+                        j = '123.123';
+                    }
+                }
+            }) | ConvertTo-GooFlattenHashtable
+        $eqs = $actual | Compare-Object -ReferenceObject $expected -Property a, b, c, d, 'e.f', 'e.g', 'e.h.i', 'e.h.j' -IncludeEqual -CaseSensitive | Select-Object -ExpandProperty SideIndicator
+
+        # Assert
+        $eqs | ForEach-Object { $_ | Should -Be '==' }
+    }
+}
